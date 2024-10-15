9daff855032e9655d8d3578690459289eafc33c0
Diego Negrette <diegonegrette42@gmail.com>
2024-10-15 23:43:48 +0200

    Added environment files

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyCQ4XXUnQp7rM9ojpJzl0O0U4MXqaplCDs",
+      authDomain: "predyc2023.firebaseapp.com",
+      databaseURL: "https://predyc2023-default-rtdb.firebaseio.com",
+      projectId: "predyc2023",
+      storageBucket: "predyc2023.appspot.com",
+      messagingSenderId: "303790869910",
+      appId: "1:303790869910:web:0bf38e4746d8cbdc05e1b7",
+      measurementId: "G-K58Y5S7FC9"
+    },
+    useEmulators: false,
+    hostUrl: "http://localhost:4201",
+    cloudFunc: "http://127.0.0.1:5001/predyc2023/us-central1"
+  };
